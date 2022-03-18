6a74011fc356a176a9d71a1e8ef46de48073d1c5
Hwiseok Seo <owl3670@gmail.com>
2022-03-18 20:49:47 +0900

    Solve some problems

---
+++ ProblemSet/4A_Watermelon.py
@@ -0,0 +1,6 @@
+num = int(input())
+num %= 2
+if num:
+    print('NO')
+else:
+    print('YES')
---
+++ ProblemSet/71A_WayTooLongWords.py
@@ -0,0 +1,8 @@
+num = int(input())
+
+for i in range(num):
+    str = input()
+    if len(str) < 10:
+        print(str)
+    else:
+        print(f"{str[0]}{len(str) - 2}{str[-1]}")
