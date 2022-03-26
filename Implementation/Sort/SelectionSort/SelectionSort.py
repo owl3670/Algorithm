1266d1edd25bcb3a257d9016c1ad9d2049663d80
owl3670 <gnltjr123@gmail.com>
2022-03-26 15:05:22 +0900

    Optimize some algorithms

--- Implementation/Sort/SelectionSort/SelectionSort.py
+++ Implementation/Sort/SelectionSort/SelectionSort.py
@@ -4,7 +4,7 @@ print("before sort", nums)
 
 for i in range(len(nums)):
     min_idx = i
-    for j in range(i, len(nums)):
+    for j in range(i+1, len(nums)):
        if nums[j] < nums[min_idx]:
            min_idx = j
     nums[i], nums[min_idx] = nums[min_idx], nums[i]
