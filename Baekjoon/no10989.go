f3673408294b43242de45d8d75773097e4861169
owl3670 <gnltjr123@gmail.com>
2022-04-05 00:04:59 +0900

    Modify some problems

--- Baekjoon/no10989.go
+++ Baekjoon/no10989.go
@@ -1,21 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	var n int
 	fmt.Scanln(&n)
 
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
 	var counting [10001]int
 	for i := 0; i < n; i++ {
 		var num int
-		fmt.Scanln(&num)
+		fmt.Fscanln(reader, &num)
 		counting[num]++
 	}
 
 	for i, count := range counting {
 		for j := 0; j < count; j++ {
-			fmt.Println(i)
+			fmt.Fprintln(writer, i)
 		}
 	}
 }
