330c2ab8d8e11a9fe72a9e579f8774340ec24f96
Hwiseok Seo <gnltjr123@gmail.com>
2022-08-13 12:48:16 +0900

    modify some code

--- Codeforces/705A_Hulk.py
+++ Codeforces/705A_Hulk.py
@@ -3,18 +3,18 @@ from sys import stdin, stdout
 input = stdin.readline
 
 def print(val):
-    stdout.write(f'${val}\n')
+    stdout.write(f'{val}\n')
     
 n = int(input())
 
 s = 'I hate'
 hate = False
 for _ in range(n - 1):
-    s = f'${s} that'
+    s = f'{s} that'
     if hate:
-        s = f'${s} I hate'
+        s = f'{s} I hate'
     else:
-        s = f'${s} I love'
+        s = f'{s} I love'
     hate = not hate
 
-print(f'${s} it')
+print(f'{s} it')
