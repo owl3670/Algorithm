cbe9d119b63c6e5697ee151078b52ce4461f40c1
owl3670 <gnltjr123@gmail.com>
2022-09-01 13:16:49 +0900

    Solving some problems

---
+++ Codeforces/25a_iq_test.py
@@ -0,0 +1,10 @@
+from sys import stdin, stdout
+
+input = stdin.readline
+def print(val):
+    stdout.write(f'{val}\n')
+
+n = int(input())
+a = list(map(int, input().split()))
+
+for idx, val in enumerate(a):
