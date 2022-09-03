4dbbb5c60f15e52445628ad5374ad15f29a1987c
Hwiseok Seo <gnltjr123@gmail.com>
2022-09-03 20:29:03 +0900

    Solve some problems

--- Codeforces/25a_iq_test.py
+++ Codeforces/25a_iq_test.py
@@ -1,11 +1,25 @@
 from sys import stdin, stdout
 
 input = stdin.readline
+
+
 def print(val):
     stdout.write(f'{val}\n')
+
 
 n = int(input())
 a = list(map(int, input().split()))
 
+even = []
+odd = []
 for idx, val in enumerate(a):
-    input
+    if val % 2 == 0:
+        even.append(idx + 1)
+    else:
+        odd.append(idx + 1)
+    if len(even) > 1 and len(odd) == 1:
+        print(odd[-1])
+        break
+    elif len(odd) > 1 and len(even) == 1:
+        print(even[-1])
+        break
