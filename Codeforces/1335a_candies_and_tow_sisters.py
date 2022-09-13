7da10ead532274f6960c13fde0439cceb215456e
owl3670 <gnltjr123@gmail.com>
2022-09-13 14:08:31 +0900

    Modify some codes

--- Codeforces/1335a_candies_and_tow_sisters.py
+++ Codeforces/1335a_candies_and_tow_sisters.py
@@ -4,11 +4,7 @@ input = stdin.readline
 def print(val):
     stdout.write(f'{val}\n')
 
-def main():
-    t = int(input())
-    for _ in range(t):
-        n = int(input())
-        print(n // 2 if n % 2 else n // 2 - 1)
-
-if __name__ == '__main__':
-    main()
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    print(n // 2 if n % 2 else n // 2 - 1)
