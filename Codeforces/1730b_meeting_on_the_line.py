8fbac6fef176c0db6b540bfcb583f09e2a6ef558
owl3670 <gnltjr123@gmail.com>
2022-09-26 13:20:18 +0900

    Solve some problems

--- Codeforces/1730b_meeting_on_the_line.py
+++ Codeforces/1730b_meeting_on_the_line.py
@@ -7,37 +7,21 @@ def main():
         x = list(map(int, stdin.readline().split()))
         times = list(map(int, stdin.readline().split()))
         dic = {}
-        m = 0
+        m = -1
+        m_idx = 0
         for i in range(n):
             if x[i] in dic:
                 dic[x[i]] = max(dic[x[i]], times[i])
             else:
                 dic[x[i]] = times[i]
-            m = max(m, times[i])
-        dic = sorted(dic.items(), key=lambda x: x[0])
-        sets = dic
-        l = len(dic)
+            if m < times[i]:
+                m = times[i]
+                m_idx = x[i]
         min_pos = 1000000000
         max_pos = -1
-        for idx, set in enumerate(sets):
-            if set[1] == m:
-                min_pos = min(min_pos, set[0])
-                max_pos = max(max_pos, set[0])
-                continue
-            else:
-                for i in range(1, l):
-                    if idx + i < l:
-                        if sets[idx + i][1]==m:
-                            temp = sets[idx + i]
-                            calc = abs((temp[0]) - set[0]) + set[1]
-                            if calc > m:
-                                min_pos = min(min_pos, set[0] + (m - set[1]))
-                    if idx - i > -1:
-                        if sets[idx - i][1]==m:
-                            temp = sets[idx - i]
-                            calc = abs((temp[0]) - set[0]) + set[1]
-                            if calc > m:
-                                max_pos = max(max_pos, set[0] - (m - set[1]))
+        for set in dic.items():
+            min_pos = min(min_pos, min(m_idx, set[0] + (m - set[1])))
+            max_pos = max(max_pos, max(m_idx, set[0] - (m - set[1])))
         print((min_pos + max_pos) / 2)
 
 if __name__ == '__main__':
