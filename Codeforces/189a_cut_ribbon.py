f1ea2b375b23936a14bd51672d9a632e7d51f8bd
owl3670 <gnltjr123@gmail.com>
2022-10-20 15:57:49 +0900

    Solve some problems

--- Codeforces/189a_cut_ribbon.py
+++ Codeforces/189a_cut_ribbon.py
@@ -4,6 +4,20 @@ def main():
     n, a, b, c = map(int, stdin.readline().split())
     li = [a, b, c]
     dp = [0] * (n + 1)
+
+    for i in range(1, n + 1):
+        if dp[i] != 0:
+            for j in li:
+                if i + j <= n:
+                    dp[i + j] = max(dp[i + j], dp[i] + 1)
+        else:
+            for j in li:
+                if i % j == 0:
+                    dp[i] = max(dp[i], i // j)
+            if dp[i] != 0:
+                for j in li:
+                    if i + j <= n:
+                        dp[i + j] = max(dp[i + j], dp[i] + 1)
     print(dp[n])
     
 
