dbfcecfe2aa3a6a310e785462492dfb0d4d2cf94
owl3670 <gnltjr123@gmail.com>
2022-11-18 16:57:45 +0900

    Solve some problems

--- Codeforces/189a_cut_ribbon.py
+++ Codeforces/189a_cut_ribbon.py
@@ -2,20 +2,13 @@ from sys import stdin
 
 def main():
     n, a, b, c = map(int, stdin.readline().split())
-    if a + b + c <= n:
-        print(3)
-        return
-    if a + b <= n:
-        print(2)
-        return
-    if a + c <= n:
-        print(2)
-        return
-    if b + c <= n:
-        print(2)
-        return
-    print(1)
-
+    max_pieces = 0
+    for i in range(0, (n // a) + 1):
+        for j in range(0, (n // b) + 1):
+            for k in range(0, (n // c) + 1):
+                if i*a + j*b + k*c == n:
+                    max_pieces = max(max_pieces, i + j + k)
+    print(max_pieces)
 
 if __name__ == '__main__':
     main()
