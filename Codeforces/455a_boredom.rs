f9341dc9880edc446ea555665fc1cf9142cbd3f2
owl3670 <gnltjr123@gmail.com>
2024-03-23 17:20:08 +0900

    + solve some problems

--- Codeforces/455a_boredom.rs
+++ Codeforces/455a_boredom.rs
@@ -1,10 +1,26 @@
 use std::io;
+use std::cmp;
 
 fn main(){
     let mut n = String::new();
     io::stdin().read_line(&mut n).expect("Read error");
+    let n: usize = n.trim().parse().expect("Parse error");
 
     let mut a = String::new();
     io::stdin().read_line(&mut a).expect("Read error");
-    let a: Vec<i32> = a.trim().split_whitespace().(|x| x.parse().expect("Parse error")).collect();
+    let a: Vec<usize> = a.trim().split_whitespace().map(|x| x.parse().expect("Parse error")).collect();
+
+    let mut cnt = vec![0; 100001];
+    for i in 0..n {
+        cnt[a[i]] += 1;
+    }
+
+    let mut dp = vec![0; 100001];
+    dp[0] = 0;
+    dp[1] = cnt[1];
+    for i in 2..100001 {
+        dp[i] = cmp::max(dp[i-1], dp[i-2] + i * cnt[i]);
+    }
+
+    println!("{}", dp[100000]);
 }
