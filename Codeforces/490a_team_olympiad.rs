fb4ad4774b3d82bc6e08a865c335218be853a99e
owl3670 <gnltjr123@gmail.com>
2025-01-08 17:19:28 +0900

    solve: 490a

--- Codeforces/490a_team_olympiad.rs
+++ Codeforces/490a_team_olympiad.rs
@@ -1,29 +1,50 @@
 use std::io;
 
+fn read_number() -> i32 {
+    let mut t = String::new();
+    io::stdin().read_line(&mut t).expect("Read error");
+    t.trim().parse::<i32>().expect("Parse error")
+}
+
+fn read_splitnumber()->Vec<i32>{
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).expect("Error");
+    return line.trim().split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect();
+}
+
 fn main(){
-    let mut n = String::new();
-    io::stdin().read_line(&mut n).expect("Read error");
-    let n: u32 = n.trim().parse().expect("Parse error");
+    let _n = read_number();
+    let t = read_splitnumber();
 
-    let mut students = String::new();
-    io::stdin().read_line(&mut a).expect("Read error");
-    let students: Vec<u32> = students.trim().split_whitespace().map(|x| x.parse().expect("Parse error")).collect();
+    let mut vec1 = Vec::new();
+    let mut vec2 = Vec::new();
+    let mut vec3 = Vec::new();
 
-    let mut result: Vec<Vec<u32>> = vec![vec![], vec![]];
-
-    for i in 0..n as usize {
-        if students[i] == 1 {
-            result[0].push(i as u32 + 1);
-        } else if students[i] == 2 {
-            result[1].push(i as u32 + 1);
-        } else if students[i] == 3 {
-            result[2].push(i as u32 + 1);
+    for i in 0..t.len() {
+        match t[i] {
+            1 => vec1.push(i + 1),
+            2 => vec2.push(i + 1),
+            3 => vec3.push(i + 1),
+            _ => ()
         }
     }
 
-    println!("{}", min);
+    let mut len_vec = Vec::new();
+    len_vec.push(vec1.len());
+    len_vec.push(vec2.len());
+    len_vec.push(vec3.len());
+
+    let mut min = 5001;
+
+    for l in len_vec {
+        if min > l {
+            min = l;
+        }
+    }
+
+    println!("{min}");
 
     for i in 0..min {
-        println!("{} {} {}", result[0][i], result[1][i], result[2][i]);
+        println!("{} {} {}", vec1[i], vec2[i], vec3[i]);
     }
 }
